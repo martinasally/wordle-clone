37d175fd4cafca93906f7952f81e49f9ec24d2d8
Martina Baydala <baydalamb@gmail.com>
2024-02-18 13:49:57 -0700

    add comments to clarify how the message fades in case it needs to change later

--- javascript.js
+++ javascript.js
@@ -6,21 +6,25 @@ error_message
 submittion.value = "";
 let submittion_value = "";
 
-
+// function to fade an element in with a given text value
+// increase the opacity by 0.1 every 30ms until the opacity is 1
 function fade_in(target, message) {
     let op = 0;
     target.style.opacity = op;
     target.textContent = message;
 
+    // execute the code inside every 30ms. Change the 30 to change the speed of the fade
     let timer = setInterval(() => {
         op += 0.1
         target.style.opacity = op;
         if (op >= 1) {
             clearInterval(timer);
         }
-    }, 30);
+    }, 30); // <-- change this to change fade speed
 }
 
+// function to fade out an element
+// decrease the opacity by 0.1 every 30ms until the opacity is 0
 function fade_out(target) {
     let op = 1;
     target.style.opacity = op;
@@ -32,9 +36,10 @@ function fade_out(target) {
             clearInterval(timer);
             target.textContent = "";
         }
-    }, 30)
+    }, 30) // <-- change this to change fade speed
 }
 
+// fade the message in slowly, keep it there for 3 seconds, then fade it out slowly
 function fade(target, message) {
     fade_in(error_message, message);
     setTimeout( () => {
@@ -46,12 +51,15 @@ submit_button.addEventListener('click', () => {
     // entered something other than letters
     if (!/^[a-zA-Z]+$/.test(submittion.value)) {
         fade(error_message, "you must enter only letters");
+    
+    // entered something with the wrong length
     } else if (!(submittion.value.length == 5)) {
         fade(error_message, "you must enter exactly 5 letters");
     }
 });
 
 
-function check_dictionary() {
-    
+// check if the user-entered word is a word in the dictionary
+function check_dictionary(word) {
+
 }
